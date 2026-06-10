df0f1d446ee07aa16a713f975581c1477200899d
LuisHRodrigues <luishenriquebr505@gmail.com>
2026-06-10 19:34:32 -0300

    aplicar as configs corretas da classe OpenApiConfig

--- src/main/java/br/edu/ifgoiano/academico/matricula_service/config/OpenApiConfig.java
+++ src/main/java/br/edu/ifgoiano/academico/matricula_service/config/OpenApiConfig.java
@@ -2,24 +2,32 @@ package br.edu.ifgoiano.academico.matricula_service.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 /**
  * Configuração do OpenAPI/Swagger.
  *
  * Após subir o serviço, a documentação interativa fica disponível em:
- *   - Swagger UI:   http://localhost:8081/swagger-ui.html
- *   - OpenAPI JSON: http://localhost:8081/v3/api-docs
+ * - Swagger UI: http://localhost:8081/swagger-ui.html
+ * - OpenAPI JSON: http://localhost:8081/v3/api-docs
  */
 @Configuration
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI matriculaServiceOpenAPI() {
-        return new OpenAPI().info(new Info()
-                .title("Matrícula Service API")
-                .description("API de criação, cancelamento e consulta de matrículas do Sistema Acadêmico Distribuído.")
-                .version("v1"));
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("/matricula").description("Via API Gateway"),
+                        new Server().url("/").description("Acesso direto ao serviço")))
+                .info(new Info()
+                        .title("Matrícula Service API")
+                        .description(
+                                "API de criação, cancelamento e consulta de matrículas do Sistema Acadêmico Distribuído.")
+                        .version("v1"));
     }
 }
